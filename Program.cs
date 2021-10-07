acd256c142e08e9b3c04a620e91da2488c71b0fc
gabriel-dev7cd <gabriel.alexandre.dev@gmail.com>
2021-10-07 16:18:31 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DioSeriesLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //EntidadeBase minhaClasse = new EntidadeBase();
+            Serie meuObj = new Serie();
+            
+
+        }
+    }
+}
